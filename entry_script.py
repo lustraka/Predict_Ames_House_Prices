ba23e150a44389bf621f56c5392842dbfde66580
Lubomir Straka <l.straka@seznam.cz>
2021-08-11 13:29:35 +0200

    Update and rename score.py to entry_script.py

---
+++ entry_script.py
@@ -0,0 +1,11 @@
+import joblib
+import os
+
+def init():
+    global model
+    model_path = os.path.join(os.environ['AZUREML_MODEL_DIR'], 'outputs/model.joblib')
+    model = joblib.load(model_path)
+
+
+def run(data):
+    return model.predict(data)
