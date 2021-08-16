28bcbf6dee98c73639afa8aad61391ace71979ef
Lubomir Straka <l.straka@seznam.cz>
2021-08-16 09:25:43 +0200

    File organization

--- Test.py
+++
@@ -1,7 +0,0 @@
-from azureml.core import Workspace, Dataset
-
-ws = Workspace.from_config()
-print(ws.name, ws.resource_group, ws.location, ws.subscription_id, sep='\n')
-
-dataset = Dataset.get_by_name(ws, name='mercari-train-data')
-print(dataset.to_pandas_dataframe().info())
